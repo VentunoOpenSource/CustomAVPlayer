bed231de01cb17c2c3aaf0fb889e5140614b9175
Kathiresan Arumugapandi <reachkathiresan@gmail.com>
2019-01-09 17:12:28 +0530

    subtitles added in player

--- CustomPlayer/ViewController.swift
+++ CustomPlayer/ViewController.swift
@@ -41,8 +41,8 @@ class VtnPlayerView {
         //16:9 mp4url
         //let mp4url = URL(string: "https://vtnpmds-a.akamaihd.net/669/17-10-2018/MMV1250715_TEN_640x360__H41QKIPR.mp4")
         
-        //let url = "http://cds.y9e7n6h3.hwcdn.net/videos/3044/03-09-2018/m3u8/Sivappu-Malli-ClipZ.m3u8"
-        let url = "http://cdnbakmi.kaltura.com/p/243342/sp/24334200/playManifest/entryId/0_uka1msg4/flavorIds/1_vqhfu6uy,1_80sohj7p/format/applehttp/protocol/http/a.m3u8"
+        let url = "http://cds.y9e7n6h3.hwcdn.net/videos/3044/03-09-2018/m3u8/Sivappu-Malli-ClipZ.m3u8"
+        //let url = "http://cdnbakmi.kaltura.com/p/243342/sp/24334200/playManifest/entryId/0_uka1msg4/flavorIds/1_vqhfu6uy,1_80sohj7p/format/applehttp/protocol/http/a.m3u8"
         
         
         //m3u8 without key
@@ -81,6 +81,31 @@ class VtnPlayerView {
         
         //Changing Quality
         mPlayerItem?.preferredPeakBitRate = false ? (512+1) : 0
+        
+        
+        if let mPlayerAsset = self.mPlayerAsset, let mPlayerItem = self.mPlayerItem {
+            for characteristic in mPlayerAsset.availableMediaCharacteristicsWithMediaSelectionOptions {
+                print("\(characteristic)")
+                
+                // Retrieve the AVMediaSelectionGroup for the specified characteristic.
+                if let group = mPlayerAsset.mediaSelectionGroup(forMediaCharacteristic: characteristic) {
+                    // Print its options.
+                    for option in group.options {
+                        print("  Option: \(option.displayName)")
+                    }
+                }
+            }
+            
+            if let group = mPlayerAsset.mediaSelectionGroup(forMediaCharacteristic: AVMediaCharacteristic.legible) {
+                let locale = Locale(identifier: "es-ES")
+                let options =
+                    AVMediaSelectionGroup.mediaSelectionOptions(from: group.options, with: locale)
+                if let option = options.first {
+                    // Select Spanish-language subtitle option
+                    mPlayerItem.select(option, in: group)
+                }
+            }
+        }
         
         pollPlayer()
         
